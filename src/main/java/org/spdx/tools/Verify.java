ee88fc7dd7b42c5e2104f143c1078f099f3410ab
Arthit Suriyawongkul <arthit@gmail.com>
2024-12-17 04:46:41 +0000

    Update src/main/java/org/spdx/tools/Verify.java
    Signed-off-by: Arthit Suriyawongkul <arthit@gmail.com>
    
    Co-authored-by: Gary O'Neall <gary@sourceauditor.com>

--- src/main/java/org/spdx/tools/Verify.java
+++ src/main/java/org/spdx/tools/Verify.java
@@ -95,7 +95,7 @@ public class Verify {
 		}
 		// separate out the warning from errors
 		List<String> warnings = new ArrayList<>();
-		List<String> errors = new ArrayList<String>();
+		List<String> errors = new ArrayList<>();
 		for (String verifyMsg : verify) {
 			if (verifyMsg.contains(" is deprecated.")) {
 				warnings.add(verifyMsg);
